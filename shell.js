9ad4f583faff0b67b5610150c797b6127f3ef9c3
Kaio Gomes <1369299@sga.pucminas.br>
2026-03-31 19:55:28 -0300

    initial

---
+++ shell.js
@@ -0,0 +1,70 @@
+// --------------------
+// WHATSAPP WEB JS
+// --------------------
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth(), // salva sessão automaticamente
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('🤖 Bot conectado!');
+});
+
+client.on('message', msg => {
+    if (msg.body === '!ping') {
+        msg.reply('pong 🏓');
+    }
+});
+
+client.initialize();
+
+
+// --------------------
+// EXPRESS (API)
+// --------------------
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// ROTA POST PARA ENVIAR MENSAGEM
+app.post('/send', async (req, res) => {
+    const { number, message } = req.body;
+
+    if (!number || !message) {
+        return res.status(400).json({
+            error: "Envie number e message no body JSON"
+        });
+    }
+
+    // Formata o número automaticamente
+    const chatId = number.includes('@c.us')
+        ? number
+        : `${number}@c.us`;
+
+    try {
+        await client.sendMessage(chatId, message);
+        res.json({
+            status: "enviado",
+            para: number,
+            mensagem: message
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+
+// ROTA POST PARA ENVIAR MENSAGEM
+app.post('/logout', async () => {
+    client.logout();
+});
+
+// Inicia o servidor
+app.listen(3000, () => console.log("API rodando na porta 3000"));
